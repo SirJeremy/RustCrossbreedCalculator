2f733bca527f87a4be1ace015f3e47793459979a
Jeremy <sirjeremycortez@gmail.com>
2020-07-18 21:09:28 -0700

    Added HistoryWindow and ViewModel

---
+++ RustCrossbreedWpf/RustCrossbreed/ViewModels/HistoryViewModel.cs
@@ -0,0 +1,24 @@
+﻿using RustCrossbreed.Models;
+using RustCrossbreed.Services;
+using System;
+using System.Collections.ObjectModel;
+
+namespace RustCrossbreed.ViewModels
+{
+    public class HistoryViewModel : ViewModelBase
+    {
+        #region Properties
+        public ObservableCollection<HistoryModel> History { get => HistoryRepo.GetAll(); }
+
+        private IRepository<HistoryModel> HistoryRepo { get; }
+
+        #endregion
+
+        #region Constructors
+        public HistoryViewModel(IRepository<HistoryModel> history)
+        {
+            HistoryRepo = history ?? throw new ArgumentNullException(nameof(history));
+        }
+        #endregion
+    }
+}
---
+++ RustCrossbreedWpf/RustCrossbreed/Views/HistoryWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RustCrossbreed.Views
+{
+    /// <summary>
+    /// Interaction logic for HistoryWindow.xaml
+    /// </summary>
+    public partial class HistoryWindow : Window
+    {
+        public HistoryWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void History_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteAll_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MoreInfo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
